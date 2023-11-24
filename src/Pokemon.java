b340e7bf26fc8d6ad2b0056c8c40f11203479032
Ryu <mendozaryu08@gmail.com>
2023-11-24 11:19:41 +0800

    Added Pokemon file op

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,16 +1,39 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Scanner;
 
 class Pokemon {
+    private static int nextId = 1;
+    private int id;
     private String name;
     private String type;
+    private int health;
+    private int experienceLevel;
+    private ArrayList<String> attacks;
 
     public Pokemon(String name, String type) {
+        this.id = nextId++;
         this.name = name;
         this.type = type;
+        this.health = 100;
+        this.experienceLevel = 0;
+        this.attacks = new ArrayList<>();
+        this.attacks.add("Tackle");
+        this.attacks.add("Growl");
     }
 
+
+    public String getId() {
+        return String.format("%04d", id);
+    }
     public String getName() {
         return name;
     }
@@ -19,16 +42,45 @@ class Pokemon {
         return type;
     }
 
+    public int getHealth() {
+        return health;
+    }
+
+    public int getExperienceLevel() {
+        return experienceLevel;
+    }
+
+    public ArrayList<String> getAttacks() {
+        return attacks;
+    }
+
+    public void addAttack(String attack) {
+        attacks.add(attack);
+    }
+
+    static void addPokemon(ArrayList<Pokemon> pokemonList, Scanner scanner) {
+        System.out.print("Enter Pokemon name: ");
+        String name = scanner.next();
+        System.out.print("Enter Pokemon type: ");
+        String type = scanner.next();
+
+        Pokemon newPokemon = new Pokemon(name, type);
+        pokemonList.add(newPokemon);
+
+        System.out.println("Pokemon added successfully!");
+    }
+
     static void showPokemonList(ArrayList<Pokemon> pokemonList) {
         if (pokemonList.isEmpty()) {
             System.out.println("No Pokemon in the list.");
         } else {
             System.out.println("Pokemon List:");
             for (Pokemon pokemon : pokemonList) {
-                System.out.println("Name: " + pokemon.getName() + ", Type: " + pokemon.getType());
+                System.out.println("#" + pokemon.getId() + " - " + pokemon.getName() + "\t Type: " + pokemon.getType());
             }
         }
     }
+
 
     static void sortByName(ArrayList<Pokemon> pokemonList) {
         Collections.sort(pokemonList, Comparator.comparing(Pokemon::getName));
@@ -39,4 +91,65 @@ class Pokemon {
         Collections.sort(pokemonList, Comparator.comparing(Pokemon::getType));
         System.out.println("Pokemon list sorted by type.");
     }
+
+    static void selectPokemon(ArrayList<Pokemon> pokemonList, Scanner scanner) {
+        System.out.print("Enter the name of the Pokemon: ");
+        String selectedName = scanner.next();
+
+        Pokemon selectedPokemon = findPokemonByName(pokemonList, selectedName);
+
+        if (selectedPokemon != null) {
+            displayPokemonDetails(selectedPokemon);
+        } else {
+            System.out.println("Pokemon not found.");
+        }
+    }
+
+    private static Pokemon findPokemonByName(ArrayList<Pokemon> pokemonList, String name) {
+        for (Pokemon pokemon : pokemonList) {
+            if (pokemon.getName().equalsIgnoreCase(name)) {
+                return pokemon;
+            }
+        }
+        return null;
+    }
+
+    private static void displayPokemonDetails(Pokemon pokemon) {
+        System.out.println("Pokemon Details:");
+        System.out.println("ID: " + pokemon.getId());
+        System.out.println("Name: " + pokemon.getName());
+        System.out.println("Type: " + pokemon.getType());
+        System.out.println("Health: " + pokemon.getHealth() + "HP");
+        System.out.println("Experience Level: " + pokemon.getExperienceLevel() + "/100");
+        System.out.println("Attacks: " + String.join(", ", pokemon.getAttacks()));
+
+        try {
+            String description = loadDescriptionFromFile(pokemon.getName());
+            System.out.println("Description: " + description);
+        } catch (IOException e) {
+            System.out.println("Error loading description: " + e.getMessage());
+        }
+    }
+
+    private static String loadDescriptionFromFile(String pokemonName) throws IOException {
+        String folderPath = "C:\\Users\\hp\\Desktop\\Ryu Files\\Codes\\Java\\PokemonManager\\src\\pokemon_descriptions";  // Assuming the folder is directly under the 'src' directory
+        String fileName = pokemonName + ".txt";
+        Path filePath = Paths.get(folderPath, fileName);
+
+        System.out.println("File Path: " + filePath);
+
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath.toFile()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            throw new IOException("Error loading description: " + filePath, e);
+        }
+
+        return content.toString();
+    }
+
+
 }
--- src/PokemonManager.java
+++ src/PokemonManager.java
@@ -3,10 +3,20 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
 
+
+
 public class PokemonManager {
     public static void main(String[] args) {
+
+
+        System.out.println("Current Working Directory: " + System.getProperty("user.dir"));
+
         ArrayList<Pokemon> pokemonList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+
+        pokemonList.add(new Pokemon("Bulbasaur", "Grass/Poison"));
+        pokemonList.add(new Pokemon("Charmander", "Fire"));
+        pokemonList.add(new Pokemon("Squirtle", "Water"));
 
         int choice;
 
@@ -16,13 +26,14 @@ public class PokemonManager {
             System.out.println("2. Show Pokemon List");
             System.out.println("3. Sort by Name");
             System.out.println("4. Sort by Type");
-            System.out.println("5. Exit");
+            System.out.println("5. Select Pokemon");
+            System.out.println("6. Exit");
             System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
 
             switch (choice) {
                 case 1:
-                    addPokemon(pokemonList, scanner);
+                    Pokemon.addPokemon(pokemonList, scanner);
                     break;
                 case 2:
                     Pokemon.showPokemonList(pokemonList);
@@ -34,27 +45,18 @@ public class PokemonManager {
                     Pokemon.sortByType(pokemonList);
                     break;
                 case 5:
+                    Pokemon.selectPokemon(pokemonList, scanner);
+                    break;
+                case 6:
                     System.out.println("Exiting the program. Goodbye!");
                     break;
                 default:
                     System.out.println("Invalid choice. Please enter a valid option.");
             }
 
-        } while (choice != 5);
+        } while (choice != 6);
 
         scanner.close();
-    }
-
-    private static void addPokemon(ArrayList<Pokemon> pokemonList, Scanner scanner) {
-        System.out.print("Enter Pokemon name: ");
-        String name = scanner.next();
-        System.out.print("Enter Pokemon type: ");
-        String type = scanner.next();
-
-        Pokemon newPokemon = new Pokemon(name, type);
-        pokemonList.add(newPokemon);
-
-        System.out.println("Pokemon added successfully!");
     }
 
 
